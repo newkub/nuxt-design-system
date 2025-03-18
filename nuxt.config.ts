0704e58615abe56d477fbede412eca90647bf8b3
newkub <newkubise@gmail.com>
2025-03-19 00:36:54 +0700

    ✨ Add UnoCSS integration with Vite and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Unocss from 'unocss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      Unocss()
+    ]
+  }
 })
