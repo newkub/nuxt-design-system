7492a1a349629f26ab9f8f3dce3500c71ec89356
Veerapong <newkubise@gmail.com>
2025-11-12 13:56:53 +0700

    chore(design-system): add root configuration

---
+++ packages/design-system/tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig([
+    {
+        entry: 'app/components.ts',
+
+      },
+      {
+        entry: 'app/composables.ts',
+      }
+])
