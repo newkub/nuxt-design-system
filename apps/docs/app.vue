65e159931a7f4b8acbc7f63df0a1ab223783016a
Veerapong <newkubise@gmail.com>
2025-11-15 09:10:05 +0700

    feat(docs): Integrate wdocs-ui and add button component

--- apps/docs/app.vue
+++ apps/docs/app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { WButton } from '@wrikka/wdocs-ui'
+</script>
+
 <template>
   <div>
     <h1>Welcome to wdocs (Nuxt Version)</h1>
+    <WButton />
   </div>
 </template>
